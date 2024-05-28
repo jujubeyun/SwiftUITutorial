0e48d8fe04eeea131437bee3751b5d860a20d5f5
normalturtle <kre0708@naver.com>
2024-05-28 12:33:09 +0900

    add Landmarks

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Juhyun Yun on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
