0041c97b699ee3409ebd4c4e141fd6ebf12a7e63
normalturtle <kre0708@naver.com>
2024-06-03 15:08:40 +0900

    feat: add model data object" "

--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main // 앱의 진입점
 struct LandmarksApp: App {
+    @State private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
         }
     }
 }
