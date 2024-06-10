9b52078f118caa64d5b650f11b6f3b94bb3f050a
normalturtle <kre0708@naver.com>
2024-06-10 15:50:15 +0900

    Apple-Frameworks: refactor FrameworkGridView

--- Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -14,13 +14,15 @@ struct FrameworkGridView: View {
                                GridItem(.flexible())]
     
     var body: some View {
-        LazyVGrid(columns: columns) {
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
         }
     }
 }
@@ -30,19 +32,19 @@ struct FrameworkGridView: View {
 }
 
 struct FrameworkTitleView: View {
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit() // give the ability to shrink if needed
                 .minimumScaleFactor(0.6) // it can shrink until 60% of original size
         }
+        .padding()
     }
 }
