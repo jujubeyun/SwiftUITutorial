32f38f42b224fc5241f24fca54a551c0f12630fa
normalturtle <kre0708@naver.com>
2024-06-11 12:57:45 +0900

    create BarcodeScanner

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Juhyun Yun on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
