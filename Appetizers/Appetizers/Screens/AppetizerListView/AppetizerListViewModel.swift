e017f0b6d90ca8d89235793a81b14263cf7eaea6
normalturtle <kre0708@naver.com>
2024-06-14 15:37:40 +0900

    Appetizers: change network call to async/await

--- Appetizers/Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
+++ Appetizers/Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class AppetizerListViewModel: ObservableObject {
+@MainActor final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
     @Published var alertItem: AlertItem?
@@ -15,27 +15,55 @@ final class AppetizerListViewModel: ObservableObject {
     @Published var isShowingDetail = false
     @Published var selectedAppetizer: Appetizer?
     
+//    func getAppetizers() {
+//        isLoading = true
+//        
+//        NetworkManager.shared.getAppetizers { result in
+//            DispatchQueue.main.async {
+//                self.isLoading = false
+//                switch result {
+//                case .success(let appetizers):
+//                    self.appetizers = appetizers
+//                case .failure(let error):
+//                    switch error {
+//                    case .invalidURL:
+//                        self.alertItem = AlertContext.invalidURL
+//                    case .invalidResponse:
+//                        self.alertItem = AlertContext.invalidResponse
+//                    case .invalidData:
+//                        self.alertItem = AlertContext.invalidData
+//                    case .unableToComplete:
+//                        self.alertItem = AlertContext.unableToComplete
+//                    }
+//                }
+//            }
+//        }
+//    }
+    
     func getAppetizers() {
         isLoading = true
         
-        NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
-                self.isLoading = false
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                case .failure(let error):
-                    switch error {
-                    case .invalidURL:
-                        self.alertItem = AlertContext.invalidURL
-                    case .invalidResponse:
-                        self.alertItem = AlertContext.invalidResponse
-                    case .invalidData:
-                        self.alertItem = AlertContext.invalidData
-                    case .unableToComplete:
-                        self.alertItem = AlertContext.unableToComplete
+        Task {
+            do {
+                appetizers = try await NetworkManager.shared.getAppetizers()
+                isLoading = false
+            } catch {
+                if let apError = error as? APError {
+                    switch apError {
+                        case .invalidURL:
+                            alertItem = AlertContext.invalidURL
+                        case .invalidResponse:
+                            alertItem = AlertContext.invalidResponse
+                        case .invalidData:
+                            alertItem = AlertContext.invalidData
+                        case .unableToComplete:
+                            alertItem = AlertContext.unableToComplete
                     }
+                } else {
+                    alertItem = AlertContext.invalidResponse // generic error
                 }
+                
+                isLoading = false
             }
         }
     }
