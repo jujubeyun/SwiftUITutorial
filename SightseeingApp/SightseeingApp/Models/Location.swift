3492d57b114a7567d89b6af74676c47c490eaae5
normalturtle <kre0708@naver.com>
2024-06-18 14:43:53 +0900

    SightseeingApp: add resources

---
+++ SightseeingApp/SightseeingApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SightseeingApp
+//
+//  Created by Juhyun Yun on 6/18/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
