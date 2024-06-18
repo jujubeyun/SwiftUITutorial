001ffa8ce9318295be066a323ab116b7a9802d21
normalturtle <kre0708@naver.com>
2024-06-18 15:38:14 +0900

    SightseeingApp: add Map to LocaionsView

--- SightseeingApp/SightseeingApp/ViewModels/LocationsViewModel.swift
+++ SightseeingApp/SightseeingApp/ViewModels/LocationsViewModel.swift
@@ -6,12 +6,30 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
     @Published var locations: [Location]
+    @Published var mapLocation: Location {
+        didSet {
+            updateRegion(location: mapLocation)
+        }
+    }
+    @Published var cameraPosition = MapCameraPosition.region(MKCoordinateRegion())
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
-        locations = LocationsDataService.locations
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        mapLocation = locations.first!
+        updateRegion(location: mapLocation)
+    }
+    
+    private func updateRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            cameraPosition = MapCameraPosition.region(MKCoordinateRegion(center: location.coordinates, span: mapSpan))
+        }
     }
 }
