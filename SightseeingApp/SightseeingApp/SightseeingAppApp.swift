1f89dcbbb4a843770912301d02e845a109ee863b
normalturtle <kre0708@naver.com>
2024-06-18 14:21:09 +0900

    create SightseeingApp

---
+++ SightseeingApp/SightseeingApp/SightseeingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightseeingAppApp.swift
+//  SightseeingApp
+//
+//  Created by Juhyun Yun on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SightseeingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
