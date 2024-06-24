444e686fc5d3495ec7f0fb63f14c2974bd78c957
normalturtle <kre0708@naver.com>
2024-06-24 10:45:01 +0900

    Tic-Tac-Toe: set up UI, create move object, check if square is occupied

--- Tic-Tac-Toe/Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/ContentView.swift
@@ -7,15 +7,62 @@
 
 import SwiftUI
 
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        player == .human ? "xmark" : "circle"
+    }
+}
+
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundStyle(.red)
+                                .opacity(0.5)
+                                .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
+                            
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundStyle(.white)
+                        }
+                        .onTapGesture {
+                            if isSquareOccupied(in: moves, forIndex: i) { return }
+                            moves[i] = Move(player: isHumansTurn ? .human : .computer, boardIndex: i)
+                            isHumansTurn.toggle()
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains { $0?.boardIndex == index }
     }
 }
 
